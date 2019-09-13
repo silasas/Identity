4031daad31e14127fef8c4fa019f5fa29cad9ce2
Silas Anastacio <silasas@gmail.com>
2019-09-13 11:47:33 -0300

    Inicio

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace Identity
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public void Auth()
+        {
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Identity.Data.Context;
+using Identity.Data.Repositories;
+using Identity.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Identity.Startup))]
+
+namespace Identity
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<DbContexto>(options =>
+            // options.UseSqlServer(Configuration.GetConnectionString("Connect")));
+
+
+            //Serviço para login
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DbContexto>()
+                .AddDefaultTokenProviders();
+
+            //Configurando senha
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireDigit = true;
+
+            });
+
+
+            services.AddScoped<IDadosEmpresa, DadosEmpresaRepository>();
+            services.AddScoped<IDadosSocio, DadosSocioRepository>();
+        }
+    }
+}
